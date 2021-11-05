e35429f5d46842c9745a4692cdfd2bd97262f7bc
Hari Kishore <vasakishore6@gmail.com>
2021-11-05 21:10:53 +0530

    date format change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,9 @@ export class AppComponent {
   // fulldate: any = [];
   // weekdates: any = [];
   weekdates: { week: any, fulldate: any }[] = [];
+  newformatdate: any= [];
   // activeLink = this.week[0];
-  background: any = undefined;
+  // background: any = undefined;
   constructor(private _route: Router) {
     // console.log("Array of week", this.week);
     // console.log("Array of week items", this.items);
@@ -73,13 +74,15 @@ export class AppComponent {
     }
 
     let curr = new Date
-    let info: any = []
+    let converteddate: any = []
     // let item = []
 
     for(let i=1; i<=7; i++) {
       let first = curr.getDate() - curr.getDay() + i
       let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
       let date = new Date(curr.setDate(first)).getUTCDate()
+      let D = new Date(day)
+      let converteddate = ((D.getDate() > 9) ? D.getDate() : ('0' + D.getDate())) + '/' + ((D.getMonth() > 8) ? (D.getMonth() + 1) : ('0' + (D.getMonth() + 1))) + '/' + D.getFullYear();
       var newdate
       if(date < 10) newdate = "0" + date;
       // let mydate = "0" + date.getDate().slice(-2)
@@ -87,12 +90,14 @@ export class AppComponent {
       // console.log("Array of day", day);
       // this.fulldate.push(day)
       this.weekdates.push({week: newdate, fulldate: day})
+      this.newformatdate.push(converteddate)
     }
     // this.week.push(info)
     // this.weekdates.push(this.week)
     // this.weekdates.push(this.fulldate)
     // console.log("Array of week", this.week);
     // console.log("Array of date", this.fulldate);
+    console.log("New date", this.newformatdate);
     console.log("Final Array with date formats for calendar view and Api", this.weekdates);
   }
 
